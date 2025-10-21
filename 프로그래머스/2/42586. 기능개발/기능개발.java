2d6baadc801c50834399522681f1817b6b52b5ab
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-10-21 17:48:12 +0900

    [level 2] Title: 기능개발, Time: 6.09 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,22 +4,20 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> progressQueue = new LinkedList<>();
         Queue<Integer> speedQueue = new LinkedList<>();
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         
-        // 가각의 쿠에 기능, 개발속도 저장
         for(int i = 0; i < progresses.length; i++){
             progressQueue.offer(progresses[i]);
             speedQueue.offer(speeds[i]);
         }
         
         while(!progressQueue.isEmpty()){
-            // 각 기능과 속도를 꺼내서 1일치 계산
             int size = progressQueue.size();
             
+            // 1일치 진행량 
             for(int i = 0; i < size; i++){
                 int progress = progressQueue.poll() + speedQueue.peek();
                 int speed = speedQueue.poll();
-                // 하루치만큼 계산된 작업을 다시 저장, 속도도 저장
                 progressQueue.offer(progress);
                 speedQueue.offer(speed);
             }
@@ -29,14 +27,13 @@ class Solution {
                 progressQueue.poll();
                 speedQueue.poll();
                 cnt++;
-                // 100이 넘어간 작업은 배포
             }
             
-            if(cnt > 0){ // 배포가 1번이상 된 경우에만 결과값에 저장
-                answer.add(cnt);   
+            if(cnt > 0){
+                result.add(cnt);   
             }
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
