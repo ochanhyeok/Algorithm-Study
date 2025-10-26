58c9db38e80a0bff7b8d6302ff33fc9923c7b394
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-10-26 13:56:53 +0900

    [level 2] Title: 더 맵게, Time: 1352.46 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,30 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int cnt = 0;
-        boolean flag = false;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 오름차순
-        
-        
-        for(int i = 0; i < scoville.length; i++){
-            pq.offer(scoville[i]);
+        for(Integer sco : scoville){
+            pq.offer(sco);
         }
         
-        
-        while(pq.peek() < K){ // 가장 작은 소코빌 지수가 K보다 크면 반복문 종료
+        while(pq.peek() < K){
             if(pq.size() < 2){
                 return -1;
             }
             
-            int minSco = pq.poll();
-            int minSco2 = pq.poll();
-
-            int newSco = minSco + (minSco2 * 2);
-
+            int first = pq.poll();
+            int second = pq.poll();
+            int newSco = first + (second * 2);
+            
             pq.offer(newSco);
             cnt++;
-        }        
+        }
         
         return cnt;
     }
