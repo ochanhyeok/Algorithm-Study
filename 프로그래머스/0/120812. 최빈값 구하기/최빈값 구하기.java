de7e89fa2689e9e15a7ab051cd32b4369c94302a
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-02 13:51:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.43 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        int maxCnt = 0;
-        int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int num : array){
+        int maxCnt = 0;   
+        int result = 0;
+
+        for(int num : array){ // map에 arr의 숫자 중복 카운트해서 저장
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
         for(int key : map.keySet()){
-            int cnt = map.get(key);
-            
+            int cnt = map.get(key); // 중복된 횟수
             if(cnt > maxCnt){
                 maxCnt = cnt;
-                answer = key;
+                result = key;
             }
         }
         
-        int maxCount = 0;
+        int maxCount = 0; // 최빈값이 몇개인지 카운트하는 변수
         for(int cnt : map.values()){
             if(cnt == maxCnt){
                 maxCount++;
             }
         }
-    
+        
         if(maxCount > 1){
             return -1;
         }
         
-        return answer;
+        return result;
     }
 }
