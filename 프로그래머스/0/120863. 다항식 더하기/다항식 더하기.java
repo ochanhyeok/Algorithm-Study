6eac829e5984cb9a8c29effcf2a2683d8db58955
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-03 09:33:22 +0900

    [level 0] Title: 다항식 더하기, Time: 0.81 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -2,42 +2,40 @@ import java.io.*;
 
 class Solution {
     public String solution(String polynomial) {
-        String[] terms = polynomial.split(" \\+ "); // 공백+로 문자열 분리
-        StringBuilder sb = new StringBuilder();
-        int sum = 0;
+        String[] terms = polynomial.split(" \\+ "); // + 기준으로 split
         int xSum = 0;
+        int sum = 0;
         
         for(String term : terms){
-            if(term.contains("x")){ // 식이 x를 포함하는 식일때
-                if(!term.equals("x")){ // 1보다 클때
-                    String number = term.replace("x", "");
-                    int num = Integer.parseInt(number);
-                    xSum += num;
-                } else{
+            if(term.contains("x")){ // term에 x관련 식
+                if("x".equals(term)){ // 계수가 1
                     xSum += 1;
+                } else{
+                    String num = term.replace("x", "");
+                    xSum += Integer.parseInt(num);
                 }
-                
-            } else{ // 상수일때
-                int num = Integer.parseInt(term);
-                sum += num;
+            } else {
+                sum += Integer.parseInt(term);
             }
         }
         
+        StringBuilder sb = new StringBuilder();
+        
         if(xSum > 0){
-            if(xSum == 1){
-                sb.append("x");
-            } else{
-                sb.append(xSum).append("x");
+            if(xSum > 1){
+                sb.append(xSum);
             }
+            sb.append("x");
         }
         
         if(sum > 0){
-            if(sb.length() > 0){
-                sb.append(" + ").append(sum);
+            if(xSum > 0){
+                sb.append(" + ").append(sum);    
             } else{
                 sb.append(sum);
             }
         }
+
         
         return sb.toString();
     }
