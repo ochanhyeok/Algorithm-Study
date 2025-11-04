ab224c11d8918fe229838c319bffecec9d85bfd3
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-04 09:19:03 +0900

    [level 0] Title: 문자열 밀기, Time: 0.47 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -2,32 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(String A, String B) {
-        int cnt = 0;
         LinkedList<String> list = new LinkedList<>();
+        int cnt = 0;
         
-        if(A.equals(B)){ // 두개의 문자열이 이미 같다면
-            return 0;
+        if(A.equals(B)) return 0;
+        
+        for(int i = 0; i < A.length(); i++){
+            list.add(String.valueOf(A.charAt(i)));
         }
         
         for(int i = 0; i < A.length(); i++){
-            String ch = String.valueOf(A.charAt(i));
-            list.add(ch);
-        }
-        
-        for(int i = 0; i < A.length(); i++){
-            String s = list.remove(A.length() - 1);
+            String s = list.removeLast();
             list.addFirst(s);
             cnt++;
+            
             StringBuilder sb = new StringBuilder();
             for(String st : list){
                 sb.append(st);
             }
-            String str = sb.toString();
             
+            String str = sb.toString();
             if(str.equals(B)){
                 return cnt;
             }
         }
+
         
         return -1;
     }
