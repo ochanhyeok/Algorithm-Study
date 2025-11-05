23135bcbd761437d2d45e41781ad30714d4482fe
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-05 09:28:20 +0900

    [level 0] Title: 등수 매기기, Time: 0.94 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -5,18 +5,17 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         int[] scores = new int[score.length];
         int[] result = new int[score.length];
-        int idx = 0;
         
         for(int i = 0; i < score.length; i++){
-            // double avg = (score[i][0] + score[i][1]) / 2;
-            int sum = score[i][0] + score[i][1];
-            list.add(sum);
-            scores[idx++] = sum;
+            scores[i] = score[i][0] + score[i][1];
+            list.add(scores[i]);
         }
         
         list.sort(Comparator.reverseOrder()); // 내림차순
         
-        for(int i = 0; i < idx; i++){
+        for(int i = 0; i < scores.length; i++){
+            // 합산한 점수의 인덱스가 내림차순한 list에서 몇번째인지
+            // indexOf는 첫인덱스 값만 찾기 때문에 중복이여도 인덱스는 같음
             result[i] = list.indexOf(scores[i]) + 1;
         }
         
