8350b163c359d9e403b670473791872b9e6c4b04
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-06 14:18:05 +0900

    [level 0] Title: 옹알이 (1), Time: 0.90 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,28 +1,18 @@
-import java.util.*;
 
 class Solution {
     public int solution(String[] babbling) {
         int cnt = 0;
-        List<String> list = new ArrayList<>();
         
         for(int i = 0; i < babbling.length; i++){
-            boolean flag = true;
-            String str = babbling[i].replace("aya", ".").replace("ye", ".")
+            babbling[i] = babbling[i].replace("aya", ".").replace("ye", ".")
                                     .replace("woo", ".").replace("ma", ".");
-            System.out.println(str);
-            int strCnt = 0;
-            for(int j = 0; j < str.length(); j++){
-                char ch = str.charAt(j);
-                if('.' == ch){
-                    strCnt++;
-                }
-                
-                System.out.println("strCnt = " + strCnt);
-            }
+            // 마지막으로 .을 ""로 변경
+            babbling[i] = babbling[i].replace(".", "");
             
-            if(strCnt == str.length()){
-                cnt++;   
-            }            
+            if(babbling[i].isEmpty()){
+                cnt++;
+            }
+   
         }
         
         return cnt;
