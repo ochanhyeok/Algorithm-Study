ba7f462ea6333e92db96ea1afcd771f792ebb6a7
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-08 22:18:50 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double n1 = num1;
+        double n2 = num2;
+        double n = (n1 / n2) * 1000.0;
+        
+
+            
+        return (int)n;
+    }
+}
