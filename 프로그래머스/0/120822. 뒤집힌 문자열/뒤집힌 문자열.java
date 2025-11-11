b8c7206fae47a99f597c923405dbe63ac45644f7
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-11 09:24:05 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String str = new StringBuilder(my_string).reverse().toString();
+        
+        return str;
+    }
+}
