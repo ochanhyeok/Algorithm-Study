34bab456bcb4409a8073d967ade33d37ced37230
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-17 09:50:00 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int fac = 1;
+        
+        for(int i = 1; i <= 11; i++){
+            fac *= i;
+            if(fac > n){
+                answer = i - 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
