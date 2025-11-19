4c7636ebc99603f21fe18e4e803bf1d66920e2f6
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-19 16:44:37 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.16 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int idx = 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 1; i <= 200; i++){
+            boolean flag = false;
+            for(int j = i; j > 0; j/=10){
+                int num = j % 10;
+                if(num == 3){
+                    flag = true;
+                    break;
+                }
+            }
+            
+            if(!flag){
+                if(i % 3 != 0){
+                    map.put(idx++, i);
+                    // System.out.println(i);
+                }   
+            }
+        }
+        
+        return map.get(n);
+    }
+}
