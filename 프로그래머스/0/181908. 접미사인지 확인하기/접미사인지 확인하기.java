e5f5e019767055a50d51f7a547a9a42bd8c90736
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-11-26 15:35:37 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        
+        for(int i = 0; i < my_string.length(); i++){
+            String str = my_string.substring(i, my_string.length());
+            if(str.equals(is_suffix)){
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
