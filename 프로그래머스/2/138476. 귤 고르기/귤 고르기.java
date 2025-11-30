ccca156af236cd50f98dfff2f79533aa2eb46279
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-01 02:01:11 +0900

    [level 2] Title: 귤 고르기, Time: 69.74 ms, Memory: 97 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int n : tangerine){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> sortedList = map.entrySet().stream().sorted((e1, e2) -> e2.getValue() - e1.getValue()).collect(Collectors.toList());    
+        
+        //sortedList.forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+        
+        for(Map.Entry<Integer, Integer> entry : sortedList){
+            int size = entry.getKey();
+            int cnt = entry.getValue();
+            
+            k -= cnt;
+            answer++;
+            if(k <= 0){
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
