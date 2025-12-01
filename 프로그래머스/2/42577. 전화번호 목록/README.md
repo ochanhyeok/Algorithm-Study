7c81f3096c32f8ce97f2292c1c39bb1f26c8103b
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-01 17:39:56 +0900

    [level 2] Title: 전화번호 목록, Time: 339.13 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 98.7 MB, 시간: 350.24 ms
+메모리: 99.7 MB, 시간: 339.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 16일 15:40:22
+2025년 12월 01일 17:39:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,12 +1,15 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        
         Arrays.sort(phone_book);
         
+        // System.out.println(Arrays.toString(phone_book));
+        
+        // contains로 하면 번호 중간에 앞에 번호가 포함되어도 true를 반환함
+        // 접두어인 경우니까 startsWith() 사용
         for(int i = 0; i < phone_book.length - 1; i++){
-            // 앞에서 정렬했기 때문에 인접행렬만 확인
             if(phone_book[i + 1].startsWith(phone_book[i])){
                 return false;
             }
