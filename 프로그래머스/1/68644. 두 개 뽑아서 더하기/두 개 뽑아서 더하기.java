a4dd37a5a186cc6e3940c0896ed11dea935580eb
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-01 14:44:44 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.80 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,31 +1,16 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        Map<Integer, Integer> map = new HashMap<>();
-        int idx = 0;
+        Set<Integer> set = new TreeSet<>();
         
         for(int i = 0; i < numbers.length; i++){
             for(int j = i + 1; j < numbers.length; j++){
                 int sum = numbers[i] + numbers[j];
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                set.add(sum);
             }
         }
         
-        // 키 기준 오름차순 정렬
-        List<Map.Entry<Integer, Integer>> sortedList = map.entrySet().stream()
-                .sorted((e1, e2) -> e1.getKey() - e2.getKey())
-                .collect(Collectors.toList());
-        
-        sortedList.forEach(entry -> System.out.println(entry.getKey() + ", " + entry.getValue()));
-        answer = new int[sortedList.size()];
-        for(Map.Entry<Integer, Integer> entry : sortedList){
-            int key = entry.getKey();
-            answer[idx++] = key;
-        }
-        
-        return answer;
+        return set.stream().mapToInt(i -> i).toArray();
     }
 }
