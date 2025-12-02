57cfe1a8d60f8548d9840d7c2bbfcaf8d6c98745
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-02 17:17:45 +0900

    [level 2] Title: 귤 고르기, Time: 93.25 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,23 +3,26 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
         
-        for(int n : tangerine){
+        for(int i = 0; i < tangerine.length; i++){
+            int n = tangerine[i];
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
         
-        List<Map.Entry<Integer, Integer>> sortedList = map.entrySet().stream().sorted((e1, e2) -> e2.getValue() - e1.getValue()).collect(Collectors.toList());    
+        List<Map.Entry<Integer, Integer>> sortedList = map.entrySet().stream().sorted((e1, e2) -> e2.getValue() - e1.getValue()).collect(Collectors.toList());
         
-        //sortedList.forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+        //sortedList.forEach(entry -> System.out.println(entry.getKey() + ", " + entry.getValue()));
+        
         
         for(Map.Entry<Integer, Integer> entry : sortedList){
             int size = entry.getKey();
             int cnt = entry.getValue();
             
-            k -= cnt;
+            k -= cnt;            
             answer++;
+            
             if(k <= 0){
                 break;
             }
