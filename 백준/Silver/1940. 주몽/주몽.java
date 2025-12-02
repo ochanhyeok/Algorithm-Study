918dcc754b253cda732df5616a09de3362e55c04
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-02 10:17:24 +0900

    [Silver IV] Title: 주몽, Time: 128 ms, Memory: 15104 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,40 +1,41 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = 0;
-
+        
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
+        
         int[] arr = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-
+        
         int leftIdx = 0;
-        int rightIdx = N - 1;
-        while(leftIdx < rightIdx) {
+        int rightIdx = arr.length - 1;
+        
+        Arrays.sort(arr);
+        
+        while(leftIdx < rightIdx){
             int sum = arr[leftIdx] + arr[rightIdx];
-
-            if (sum == M) {
+            
+            if(sum == M){
                 cnt++;
                 leftIdx++;
                 rightIdx--;
-            } else if(sum < M){
+            }
+            // 1, 2, 3, 4, 5, 7
+            else if(sum < M){
                 leftIdx++;
             } else {
                 rightIdx--;
             }
-
         }
-
+        
         System.out.println(cnt);
     }
 }
