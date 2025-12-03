b25f606cdd2f15278ff3cb2268ac5bbe502df3f7
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-03 22:32:29 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 21.94 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 30.23 ms
+메모리: 82.8 MB, 시간: 21.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 23일 19:00:23
+2025년 12월 03일 22:32:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,31 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         
-        // 0 세팅
         for(int i = 0; i < bridge_length; i++){
-            bridge.offer(0);
+            q.offer(0);
         }
         
-        int sum = 0;
         int time = 0;
         int idx = 0;
-        
+        int cur_weight = 0;
         while(idx < truck_weights.length){
-            sum -= bridge.poll();
             time++;
-            
-            if(sum + truck_weights[idx] <= weight){
-                bridge.offer(truck_weights[idx]);
-                sum += truck_weights[idx];
+            cur_weight -= q.poll();
+               
+            if(cur_weight + truck_weights[idx] <= weight){
+                q.offer(truck_weights[idx]);
+                cur_weight += truck_weights[idx];
                 idx++;
-            }
-            else{
-                bridge.offer(0);
+            } else {
+                q.offer(0);
             }
         }
-        
+
+        // whlie문이 끝나면 마지막트럭은 다리에 막 들어선 상태기 때문에 다리길이만큼 더해줘야 함
         return time + bridge_length;
     }
 }
