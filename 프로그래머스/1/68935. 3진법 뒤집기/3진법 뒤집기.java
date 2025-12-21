6b0ce1824661b6da81e83fef5f14b5edea1057e0
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-21 13:35:21 +0900

    [level 1] Title: 3진법 뒤집기, Time: 1.79 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,25 @@
 class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        // 3진법 변환 후 reverse
-        String trans3 = Integer.toString(n, 3);
-        sb.append(trans3).reverse();
+        int result = 0;
+        String temp = "";
         
-        String temp = sb.toString();
-        // 10진법 변환
-        int answer = Integer.parseInt(temp, 3);
+        for(int i = n; i > 0; i/=3){
+            int j = i % 3;
+            temp += String.valueOf(j);
+        }
+
+        long num = Long.parseLong(temp);
+        int cnt = 0;
         
-        return answer;
+        for(long i = num; i > 0; i/=10){
+            long j = i % 10;
+            for(long k = 0; k < cnt; k++){
+                j *= 3;
+            }
+            cnt++;
+            result += j;
+        }
+        
+        return result;
     }
 }
