fb6411f844248f1ae05f99c3ec0469d234dda0e9
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-01-02 21:42:28 +0900

    [level 2] Title: 더 맵게, Time: 1527.86 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -5,19 +5,18 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int cnt = 0;
         
-        for(Integer sco : scoville){
-            pq.offer(sco);
+        for(int i = 0; i < scoville.length; i++){
+            pq.offer(scoville[i]);
         }
         
-        
-        while(pq.peek() < K){
+        while(!pq.isEmpty() && pq.peek() < K){
             if(pq.size() < 2){
                 return -1;
             }
             
-            int first = pq.poll();
-            int second = pq.poll();
-            int newSco = first + (second * 2);
+            int sco1 = pq.poll();
+            int sco2 = pq.poll();
+            int newSco = sco1 + (sco2 * 2);
             
             pq.offer(newSco);
             cnt++;
