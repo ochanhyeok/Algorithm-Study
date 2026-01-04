b2f9fe17fd3b20c0f6942fb19ff1bbc5e088059c
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-01-04 23:47:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,33 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		String[] minus = str.split("-");
+		int sum = 0;
+
+		String[] first = minus[0].split("\\+");
+		for (String num : first) {
+			sum += Integer.parseInt(num);
+		}
+
+		// 내부 + 들을 다 더한 뒤 한번에 빼기
+		for (int i = 1; i < minus.length; i++) {
+			String[] plus = minus[i].split("\\+");
+			int temp = 0;
+			for (String num : plus) {
+				temp += Integer.parseInt(num);
+			}
+			sum -= temp;
+		}
+
+		System.out.println(sum);
+	}
+}
