f430ae4d4ed2d2df945d6568ee97db8ed7b20d26
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-02-12 15:39:59 +0900

    [level 4] Title: 징검다리, Time: 38.17 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
-        int answer = 0;
+        int result = 0;
+        
         Arrays.sort(rocks);
         
         int left = 0;
         int right = distance;
-
+        
         while(left <= right){
             int mid = (left + right) / 2;
             
-            if(canRemove(rocks, n, mid, distance)){
-                answer = mid;
-                left = mid + 1;                
+            if(canRemove(rocks, mid, n, distance)){
+                result = mid;
+                left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
         
-        return answer;
+        return result;
     }
     
-    static boolean canRemove(int[] rocks, int n, int mid, int distance){
+    static boolean canRemove(int[] rocks, int mid, int n, int distance){
         int removed = 0;
         int prev = 0;
         
@@ -40,5 +41,4 @@ class Solution {
         
         return removed <= n;
     }
-    
 }
