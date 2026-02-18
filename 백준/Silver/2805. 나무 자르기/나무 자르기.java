5451d544665b4e8c10827bfe1daf7f1120b15ad8
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-02-18 19:39:48 +0900

    [Silver II] Title: 나무 자르기, Time: 588 ms, Memory: 168924 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] woods = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			woods[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(woods);
+
+		int left = 0;
+		int right = woods[N - 1];
+
+		int result = 0;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			// System.out.println("======================");
+			// System.out.println("mid = " + mid);
+			if (canCut(woods, mid, M)) {
+				result = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+	static boolean canCut(int[] woods, int mid, int M) {
+		long totalCut = 0;
+
+		for (int wood : woods) {
+			int cutLength = wood - mid;
+			// System.out.println("cutLength = " + cutLength);
+			if (cutLength > 0) {
+				totalCut += cutLength;
+			}
+		}
+
+		return totalCut >= M;
+	}
+}
