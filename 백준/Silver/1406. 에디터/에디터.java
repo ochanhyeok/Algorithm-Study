9969d971de82551650dd15794c981ff33d96d8bf
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-02-23 00:32:44 +0900

    [Silver II] Title: 에디터, Time: 684 ms, Memory: 112460 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,5 +1,4 @@
 
-
 import java.util.*;
 import java.io.*;
 
@@ -9,39 +8,45 @@ public class Main{
 		StringTokenizer st;
 
 		String str = br.readLine();
-		StringBuilder editor = new StringBuilder(str);
-
-		int curIdx = str.length();
-
+		StringBuilder editor = new StringBuilder();
+		
 		int N = Integer.parseInt(br.readLine());
 
-		// System.out.println(String.join("", editor));
+		Deque<Character> left = new ArrayDeque<>();
+		Deque<Character> right = new ArrayDeque<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			left.addLast(str.charAt(i));
+		}
 
 		for(int i = 0; i < N; i++){
 			st = new StringTokenizer(br.readLine());
 			String cmd = st.nextToken();
 
-			if("P".equals(cmd)){ // 문자를 커서 왼쪽에 추가
-				String letter = st.nextToken();
-				editor.insert(curIdx, letter);
-				curIdx++;
-			} else if("L".equals(cmd)){ // 커서를 왼쪽으로 한 칸 옮김
-				if(curIdx > 0){
-					curIdx--;
+			if ("P".equals(cmd)) { // 문자를 커서 왼쪽에 추가
+				char c = st.nextToken().charAt(0);
+				left.addLast(c);
+			} else if ("L".equals(cmd)) { // 커서를 왼쪽으로 한 칸 옮김
+				if (!left.isEmpty()) {
+					right.addLast(left.removeLast());
 				}
-			} else if("D".equals(cmd)){ // 커서를 오른쪽으로 한 칸 옮김
-				if (curIdx < editor.length()) {
-					curIdx++;
+			} else if ("D".equals(cmd)) { // 커서를 오른쪽으로 한 칸 옮김
+				if (!right.isEmpty()) {
+					left.addLast(right.removeLast());
 				}
-			} else if("B".equals(cmd)){ // B 커서 왼쪽에 있는 문자를 삭제
-				if (curIdx > 0) {
-					editor.deleteCharAt(curIdx - 1);
-					curIdx--;
+			} else if ("B".equals(cmd)) { // B 커서 왼쪽에 있는 문자를 삭제
+				if (!left.isEmpty()) {
+					left.removeLast();
 				}
 			}
-			// System.out.print("cmd: " + cmd + ", curIdx = " + curIdx);
-			// System.out.println(", editor: " + String.join("", editor));
+		}
 
+		for (char c : left) {
+			editor.append(c);
+		}
+
+		while (!right.isEmpty()) {
+			editor.append(right.removeLast());
 		}
 
 		System.out.println(editor.toString());
