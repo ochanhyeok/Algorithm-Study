606c25f4b8b951c99803b6c941ccbb9db4486b49
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-02-27 08:31:41 +0900

    [Silver III] Title: 두 수의 합, Time: 232 ms, Memory: 26428 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,46 +1,41 @@
+import java.io.*;
+import java.util.*;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int cnt = 0;
-
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int x = Integer.parseInt(br.readLine());
-
-		Arrays.sort(arr);
-
-		int left = 0;
-		int right = n - 1;
-
-		while (left < right) {
-			int sum = arr[left] + arr[right];
-
-			if (sum == x) {
-				cnt++;
-				left++;
-				right--;
-			} else if (sum > x) {
-				right--;
-			} else {
-				left++;
-			}
-		}
-
-		System.out.println(cnt);
-	}
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int x = Integer.parseInt(br.readLine());
+        
+        Arrays.sort(arr);
+        
+        int left = 0;
+        int right = n - 1;
+        
+        int cnt = 0;
+        while(left < right){
+            int sum = arr[left] + arr[right];
+            
+            if(sum == x){
+                cnt++;
+                left++;
+                right--;
+            } else if(sum > x){
+                right--;
+            } else {
+                left++;
+            }
+        }
+        
+        System.out.println(cnt);
+    }
 }
