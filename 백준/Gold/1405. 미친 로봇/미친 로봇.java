4df8c5318c2385b611356ac08d2b02c6b90a1cb6
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-03-16 10:09:55 +0900

    [Gold IV] Title: 미친 로봇, Time: 128 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.java
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 public class Main{
     
+    static int N;
     static double answer;
-    static boolean[][] visited = new boolean[29][29];
+    static double prob[] = new double[4];
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static double[] prob;
-    static int N;
+    static boolean[][] visited = new boolean[29][29];
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         N = Integer.parseInt(st.nextToken());
-        prob = new double[4];
+        
         for(int i = 0; i < 4; i++){
             prob[i] = Integer.parseInt(st.nextToken()) / 100.0;
         }
@@ -35,6 +35,10 @@ public class Main{
         for(int d = 0; d < 4; d++){
             int nx = x + dx[d];
             int ny = y + dy[d];
+            
+            if(nx < 0 || nx >= 29 || ny < 0 || ny >= 29){
+                continue;
+            }
             
             if(!visited[nx][ny] && prob[d] > 0){
                 visited[nx][ny] = true;
