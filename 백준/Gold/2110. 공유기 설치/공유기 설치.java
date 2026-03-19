17c6649e5ba5e19b83df0131b4811cdb58223962
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-03-19 18:09:34 +0900

    [Gold IV] Title: 공유기 설치, Time: 188 ms, Memory: 28288 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,55 +1,50 @@
-
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int result = 0;
-
-		int N = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-
-		int[] houses = new int[N];
-
-		for(int i = 0; i < N; i++){
-			houses[i] = Integer.parseInt(br.readLine());
-		}
-
-		Arrays.sort(houses);
-
-		int left = 1;
-		int right = houses[N - 1] - houses[0];
-
-		while(left <= right){
-			int mid = (left + right) / 2;
-
-			if (canWifi(houses, mid, C)) {
-				result = mid;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-
-		System.out.println(result);
-	}
-
-	private static boolean canWifi(int[] houses, int mid, int C) {
-		int prev = houses[0];
-		int wifiCnt = 1;
-
-		for(int house : houses){
-			int len = house - prev;
-
-			if(len >= mid){
-				wifiCnt++;
-				prev = house;
-			}
-		}
-
-		return wifiCnt >= C;
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] houses = new int[N];
+        for(int i = 0; i < N; i++){
+            houses[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(houses);
+        
+        int left = 1;
+        int right = houses[N - 1] - houses[0];
+        
+        int answer = 0;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            
+            if(canWifi(houses, mid, M)){
+                answer = mid;
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(answer);
+    }
+    
+    static boolean canWifi(int[] houses, int mid, int M){
+        int wifiCnt = 1;
+        int prevHouse = houses[0];
+        
+        for(int house : houses){
+            if(house - prevHouse >= mid){
+                wifiCnt++;
+                prevHouse = house;
+            }
+        }
+        
+        return wifiCnt >= M;
+    }
 }
