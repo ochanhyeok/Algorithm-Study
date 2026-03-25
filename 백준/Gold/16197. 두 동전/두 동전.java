9fc71abd5e725f76dbe210a9c847763dce677083
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-03-26 08:56:22 +0900

    [Gold IV] Title: 두 동전, Time: 72 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.java
+++ 백준/Gold/16197. 두 동전/두 동전.java
@@ -7,7 +7,7 @@ public class Main{
     static char[][] grid;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    
+   
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,13 +31,16 @@ public class Main{
         System.out.println(result);
     }
     
-    static int bfs(int r1, int c1, int r2, int c2, int depth){
-        boolean[][][][] visited = new boolean[N][M][N][M];
+    static int bfs(int r1, int c1, int r2, int c2, int step){
         Queue<int[]> q = new LinkedList<>();
+        boolean[][][][] visited = new boolean[N][M][N][M];
         
-        q.offer(new int[]{r1, c1, r2, c2, 0});
+        q.add(new int[]{r1, c1, r2, c2, 0});
+        visited[r1][c1][r2][c2] = true;
+        
         while(!q.isEmpty()){
             int[] cur = q.poll();
+            
             if(cur[4] >= 10){
                 continue;
             }
@@ -49,10 +52,8 @@ public class Main{
                 boolean out1 = nr1 < 0 || nr1 >= N || nc1 < 0 || nc1 >= M;
                 boolean out2 = nr2 < 0 || nr2 >= N || nc2 < 0 || nc2 >= M;
                 
-                if(out1 && out2){
-                    continue;
-                }
-                if(out1 || out2){
+                if(out1 && out2) continue;
+                if(out1 || out2) {
                     return cur[4] + 1;
                 }
                 
@@ -67,11 +68,10 @@ public class Main{
                 
                 if(!visited[nr1][nc1][nr2][nc2]){
                     visited[nr1][nc1][nr2][nc2] = true;
-                    q.offer(new int[]{nr1, nc1, nr2, nc2, cur[4] + 1});
+                    q.add(new int[]{nr1, nc1, nr2, nc2, cur[4] + 1});
                 }
             }
         }
-        
         return -1;
     }
 }
