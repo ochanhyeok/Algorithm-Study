eadb6444ef7c6b007868f12c4e8eacbf15ab9802
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-04-07 15:21:48 +0900

    [Gold IV] Title: 공유기 설치, Time: 296 ms, Memory: 28724 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,31 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+    
+    static int N, M;
+    static int[] houses;
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         
-        int[] houses = new int[N];
+        houses = new int[N];
+        
         for(int i = 0; i < N; i++){
             houses[i] = Integer.parseInt(br.readLine());
         }
         
         Arrays.sort(houses);
         
+        int answer = 0;
         int left = 1;
         int right = houses[N - 1] - houses[0];
         
-        int answer = 0;
         while(left <= right){
             int mid = (left + right) / 2;
             
-            if(canWifi(houses, mid, M)){
+            if(canWifi(mid)){
                 answer = mid;
                 left = mid + 1;
-            } else{
+            } else {
                 right = mid - 1;
             }
         }
@@ -34,7 +39,7 @@ public class Main{
         System.out.println(answer);
     }
     
-    static boolean canWifi(int[] houses, int mid, int M){
+    static boolean canWifi(int mid){
         int wifiCnt = 1;
         int prevHouse = houses[0];
         
