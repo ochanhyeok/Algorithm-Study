cc4380db4814ebf2e1d60ff6d11fdd97e2fe9f19
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-02 22:36:32 +0900

    [level 2] Title: 의상, Time: 16.43 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,16 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(String[] cloth : clothes){
             String type = cloth[1];
             map.put(type, map.getOrDefault(type, 0) + 1);
         }
         
-        int answer = 1;
-        for(int cnt : map.values()){
-            answer *= (cnt + 1);
+        map.forEach((a, b) -> System.out.println(a + ", " + b));
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            answer *= entry.getValue() + 1;
         }
         
         return answer - 1;
