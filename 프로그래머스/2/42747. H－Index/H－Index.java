f18ad0dae2a069f790609a3dd381ee1b271a7473
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-06 18:10:21 +0900

    [level 2] Title: H-Index, Time: 1.55 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -14,23 +14,23 @@ class Solution {
             
             if(canUse(citations, mid)){
                 answer = mid;
-                right = mid - 1;
-            } else {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         
         return answer;
     }
-    
-    static boolean canUse(int[] citations, int mid){
+    // 0 1 3 5 6
+    boolean canUse(int[] citations, int mid){
         int cnt = 0;
-        for(int ct : citations){
-            if(ct > mid){
+        for(int ci : citations){
+            if(ci >= mid){
                 cnt++;
             }
         }
         
-        return cnt <= mid;
+        return cnt >= mid;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
 
 ### 성능 요약
 
-메모리: 83.1 MB, 시간: 1.57 ms
+메모리: 93.4 MB, 시간: 1.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 22일 23:40:25
+2026년 05월 06일 18:10:17
 
 ### 문제 설명
 
