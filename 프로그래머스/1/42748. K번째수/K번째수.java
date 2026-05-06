ca0e48f2f30b3b729d36aa3b999f779c2caff502
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-06 15:49:12 +0900

    [level 1] Title: K번째수, Time: 3.07 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         
-        for(int i = 0; i < commands.length; i++){
+        for(int k = 0; k < commands.length; k++){
+            int len = commands[k][1] - commands[k][0] + 1;
+            int[] arr = new int[len];
             int idx = 0;
-            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
-            for(int j = commands[i][0] - 1; j < commands[i][1]; j++){
-                arr[idx++] = array[j];
+            
+            for(int i = commands[k][0] - 1; i < commands[k][1]; i++){
+                arr[idx++] = array[i];
             }
+            
             Arrays.sort(arr);
             
-            list.add(arr[commands[i][2] - 1]);
-            // System.out.println(Arrays.toString(arr) + ", " + arr[commands[i][2] - 1]);
+            result.add(arr[commands[k][2] - 1]);
         }
         
-        return list.stream().mapToInt(i -> i).toArray();
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 3.36 ms
+메모리: 73.7 MB, 시간: 3.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 01일 16:42:37
+2026년 05월 06일 15:49:08
 
 ### 문제 설명
 
