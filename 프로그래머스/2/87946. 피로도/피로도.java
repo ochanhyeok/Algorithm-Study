5a18eda960aa624612a28dc1d3aeded29c4672d6
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-09 21:39:53 +0900

    [level 2] Title: 피로도, Time: 1.64 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    
+    boolean[] visited;
+    int answer = 0;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        
+        
+        return answer;
+    }
+    
+    void dfs(int k, int[][] dungeons, int cnt){
+        answer = Math.max(cnt, answer);
+        
+        for(int i = 0; i < dungeons.length; i++){
+            if(!visited[i] && k >= dungeons[i][0]){
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
