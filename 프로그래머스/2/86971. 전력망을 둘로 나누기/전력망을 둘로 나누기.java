a7910ac196cced71f1d8608f1ca57cf12d2d8241
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-13 20:34:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     
-    static List<Integer>[] graph;
+    List<Integer>[] graph;
     
     public int solution(int n, int[][] wires) {
         int answer = n;
@@ -19,34 +19,27 @@ class Solution {
             graph[b].add(a);
         }
         
-        // 간선 하나씩 끊어보기
         for(int i = 0; i < wires.length; i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
             
-            // 간선 제거
             graph[v1].remove(Integer.valueOf(v2));
             graph[v2].remove(Integer.valueOf(v1));
             
-            // DFS로 한쪽 전력망 크기 구하기
             boolean[] visited = new boolean[n + 1];
             int cnt = dfs(1, visited);
-            
-            // 두 전력망 차이 계산
             int diff = Math.abs(cnt - (n - cnt));
-            answer = Math.min(answer, diff);
             
-            // 간선 복구
+            answer = Math.min(diff, answer);
+            
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
         
-        //System.out.println(Arrays.toString(graph));
-        
         return answer;
     }
     
-    private int dfs(int node, boolean[] visited){
+    int dfs(int node, boolean[] visited){
         visited[node] = true;
         int cnt = 1;
         
