7b88e767b7630fad5df6e0291e77679376182348
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-21 11:25:10 +0900

    [level 2] Title: 프로세스, Time: 2.44 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,35 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> queue = new LinkedList<>();
-        int result = 0;
+        int answer = 0;
+        Queue<int[]> q = new LinkedList<>();
         
-        // 큐에 우선순위아 인덱스 삽입
         for(int i = 0; i < priorities.length; i++){
-            queue.offer(new int[]{priorities[i], i});
+            q.add(new int[]{priorities[i], i});
         }
         
-        while(!queue.isEmpty()){
-            int cur[] = queue.poll(); // 현재 우선순위
-            boolean hasHighPriority = false;
+        // q.forEach((a, b) -> System.out.println(a + ", " + b));
+        
+        while(!q.isEmpty()){
+            boolean flag = false;
+            int[] cur = q.poll();
             
-            for(int[] item : queue){ // 큐 탐색해서 가장 높은 우선순위 찾기
+            
+            for(int[] item : q){
                 if(cur[0] < item[0]){
-                    hasHighPriority = true;
+                    flag = true;
                 }
             }
             
-            if(hasHighPriority){ // cur보다 큰 우선순위가 있다면
-                queue.offer(cur); // 큐 뒤에 다시 추가    
-            }
-            else{ // cur의 우선순위가 가장 크다면
-                result++;
-                if(location == cur[1]){ // 찾고자하는 loc이랑 인덱스가 같으면
-                    return result;
+            if(flag){
+                q.add(cur);
+            } else {
+                answer++;
+                if(location == cur[1]){
+                    return answer;
                 }
             }
         }
         
-        return result;
+        return answer;
     }
 }
