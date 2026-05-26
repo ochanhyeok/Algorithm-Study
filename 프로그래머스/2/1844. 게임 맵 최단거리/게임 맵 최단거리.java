fe4b42d09bb59b31dbe7e0deeb7784a8bdbd9b72
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-26 13:44:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.10 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,51 +2,45 @@ import java.util.*;
 
 class Solution {
     
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
-    int n, m;
+    static int[][] dist;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N, M;
     
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
+        N = maps.length;
+        M = maps[0].length;
+        dist = new int[N][M];
         
         return bfs(maps);
     }
     
-    int bfs(int[][] maps){       
+    static int bfs(int[][] maps){
         Queue<int[]> q = new LinkedList<>();
-        int[][] dist = new int[n][m];
         q.offer(new int[]{0, 0});
         dist[0][0] = 1;
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
+            int x = cur[0], y = cur[1];
             
             for(int d = 0; d < 4; d++){
                 int nx = x + dx[d];
                 int ny = y + dy[d];
                 
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m){
-                    continue;
-                }
-                if(maps[nx][ny] == 0){
-                    continue;
-                }
-                if(dist[nx][ny] != 0){
-                    continue;
-                }
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
                 
-                dist[nx][ny] = dist[x][y] + 1;
-                q.offer(new int[]{nx, ny});
+                if(dist[nx][ny] == 0 && maps[nx][ny] == 1){
+                    q.offer(new int[]{nx, ny});
+                    dist[nx][ny] = dist[x][y] + 1;
+                }
             }
         }
         
-        if(dist[n - 1][m - 1] == 0){
+        if(dist[N - 1][M - 1] == 0){
             return -1;
         }
         
-        return dist[n - 1][m - 1];
+        return dist[N - 1][M - 1];
     }
 }
