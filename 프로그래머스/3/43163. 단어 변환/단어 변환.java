d69f2170bc92f8b3ea70dac24c3c5ab9d5049ed8
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-28 01:23:19 +0900

    [level 3] Title: 단어 변환, Time: 0.14 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    
+    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
+    
+    public int solution(String begin, String target, String[] words) {    
+        visited = new boolean[words.length];
+        
+        dfs(begin, begin, target, words, 0);
+        
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+    
+    static void dfs(String begin, String cur, String target, String[] words, int cnt){
+        if(cur.equals(target)){
+            answer = Math.min(answer, cnt);
+            return;
+        }
+        
+        for(int i = 0; i < words.length; i++){
+            if(!visited[i] && canConvert(cur, words[i])){
+                visited[i] = true;
+                dfs(begin, words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    static boolean canConvert(String a, String b){
+        int diff = 0;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) diff++;
+        }
+        return diff == 1;
+    }
+}           
