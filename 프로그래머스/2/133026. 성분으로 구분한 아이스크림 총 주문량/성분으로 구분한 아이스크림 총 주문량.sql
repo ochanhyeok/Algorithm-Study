57718bc550585571870629c57f6d4499ae789183
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-06-08 20:11:58 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT ii.ingredient_type, sum(fh.total_order)
+from first_half fh
+join icecream_info ii on fh.flavor = ii.flavor
+group by ii.ingredient_type
+order by fh.total_order asc;
