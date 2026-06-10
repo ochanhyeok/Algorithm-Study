730283174075fcd76de5c3ca14e74ab85b84c244
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-06-10 14:32:47 +0900

    [level 3] Title: 등굣길, Time: 0.40 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,25 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int MOD = 1_000_000_007;
-        int[][] dp = new int[n + 1][m + 1];
+        int[][] dp = new int[m + 1][n + 1];
         
         for(int[] p : puddles){
-            dp[p[1]][p[0]] = -1;
+            dp[p[0]][p[1]] = -1;
         }
         
         dp[1][1] = 1;
         
-        for(int y = 1; y <= n; y++){
-            for(int x = 1; x <= m; x++){
-                if(dp[y][x] == -1){
-                    dp[y][x] = 0;
+        for(int x = 1; x <= m; x++){
+            for(int y = 1; y <= n; y++){
+                if(dp[x][y] == -1){
+                    dp[x][y] = 0;
                     continue;
                 }
-                if(x == 1 && y == 1) continue;
-                dp[y][x] = (dp[y - 1][x] + dp[y][x - 1]) % MOD;
+                if(x == 1 && y == 1){
+                    continue;
+                }
+                
+                dp[x][y] = (dp[x][y - 1] + dp[x - 1][y]) % MOD;
             }
         }
         
-        return dp[n][m];
+        return dp[m][n];
     }
 }
