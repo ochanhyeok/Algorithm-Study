c76b75e492a273da9d8beba00cf7d92666f1be72
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-06-12 15:59:43 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT ugu.user_id, 
+    ugu.nickname, 
+    concat(ugu.city, ' ', ugu.street_address1, ' ', ugu.street_address2) as 전체주소,
+    concat(substring(ugu.tlno, 1, 3), '-',
+          substring(ugu.tlno, 4, 4), '-',
+          substring(ugu.tlno, 8, 4)) as 전화번호
+from used_goods_board ugb
+join used_goods_user ugu on ugb.writer_id = ugu.user_id
+group by ugu.user_id
+having count(*) >= 3
+order by ugu.user_id desc;
