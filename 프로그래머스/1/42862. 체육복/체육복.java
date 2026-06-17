3060034c9846cc27e680292e39f8f7ae033b457d
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-06-17 21:02:35 +0900

    [level 1] Title: 체육복, Time: 0.30 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -5,46 +5,40 @@ class Solution {
         int answer = 0;
         int[] clothes = new int[n + 1];
         
-        // Arrays.sort(lost);
-        // Arrays.sort(reserve);
+        Arrays.fill(clothes, 1);
         
-        for(int i = 1; i <= n; i++){
-            clothes[i] = 1;
+        for(int l : lost){
+            clothes[l]--;
         }
         
-        for(int i = 0; i < reserve.length; i++){
-            clothes[reserve[i]] += 1;
+        for(int r : reserve){
+            clothes[r]++;
         }
-        
-        for(int i = 0; i < lost.length; i++){
-            clothes[lost[i]] -= 1;
-        }
+        System.out.println(Arrays.toString(clothes));
         
         for(int i = 1; i <= n; i++){
             if(clothes[i] == 0){
                 if(i - 1 >= 1 && clothes[i - 1] == 2){
-                    clothes[i] += 1;
-                    clothes[i - 1] -= 1;
+                    clothes[i - 1] = 1;
+                    clothes[i] = 1;
                 }
             }
             
             if(clothes[i] == 0){
                 if(i + 1 <= n && clothes[i + 1] == 2){
-                    clothes[i] += 1;
-                    clothes[i + 1] -= 1;
+                    clothes[i + 1] = 1;
+                    clothes[i] = 1;
                 }
             }
         }
         
-        
-        
-        // System.out.println(Arrays.toString(clothes));
-        
+        System.out.println(Arrays.toString(clothes));
         for(int i = 1; i <= n; i++){
             if(clothes[i] >= 1){
                 answer++;
             }
         }
+        
         
         return answer;
     }
