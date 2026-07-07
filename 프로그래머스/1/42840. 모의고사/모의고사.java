3858d1d32f3f0742d940903469bc2a347f716079
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-07-07 23:57:37 +0900

    [level 1] Title: 모의고사, Time: 2.19 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,29 +6,30 @@ class Solution {
         int[] supo1 = {1, 2, 3, 4, 5};
         int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
         int len1 = supo1.length;
         int len2 = supo2.length;
         int len3 = supo3.length;
         
         int idx = 0;
-        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        int[] cnt = new int[3];
         for(int ans : answers){
             if(ans == supo1[idx % len1]){
-                cnt1++;
+                cnt[0]++;
             }
             if(ans == supo2[idx % len2]){
-                cnt2++;
+                cnt[1]++;
             }
             if(ans == supo3[idx % len3]){
-                cnt3++;
+                cnt[2]++;
             }
             idx++;
         }
-        int max = Math.max(cnt1, Math.max(cnt2, cnt3));
+        int max = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
         
-        if(cnt1 == max) result.add(1);
-        if(cnt2 == max) result.add(2);
-        if(cnt3 == max) result.add(3);
+        for(int i = 0; i < 3; i++){
+            if(cnt[i] == max) result.add(i + 1);
+        }
         
         return result.stream().mapToInt(i -> i).toArray();
     }
